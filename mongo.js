54227c62ef6db83c82a9665dfe81324d3b488546
MIHIR890 <mihirpandit4850@gmail.com>
2025-08-12 21:35:49 +0530

    mongo.uri

--- mongo.js
+++ mongo.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load .env locally
+
 
 // MongoDB connection string
-const mongoURI = "mongodb://localhost:27017/BookManagement"; // Replace with your MongoDB URI
+// const mongoURI = "mongodb://localhost:27017/BookManagement"; // Replace with your MongoDB URI
+
 mongoose.set("strictQuery", true);
 
 // Function to connect to the database
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(mongoURI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
